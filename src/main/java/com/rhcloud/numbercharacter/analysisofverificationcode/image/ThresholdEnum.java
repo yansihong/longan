2eb482c05dea668e656b9fb063e80e5356fbc1b7
yansihong <yanhaisihong@hotmail.com>
2014-01-11 20:05:31 +0800

    二值化枚举

---
+++ src/main/java/com/rhcloud/numbercharacter/analysisofverificationcode/image/ThresholdEnum.java
@@ -0,0 +1,10 @@
+package com.rhcloud.numbercharacter.analysisofverificationcode.image;
+
+public enum ThresholdEnum {
+
+	MeanThreshold, PTileThreshold, MinimumThreshold, IntermodesThreshold,
+
+	IterativeBestThreshold, OSTUThreshold, OneDMaxEntropyThreshold, MomentPreservingThreshold,
+	
+	KittlerMinError
+}
